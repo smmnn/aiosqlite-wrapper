cc22965e79968d25576522be08f4de3792914e0e
simon <92829233+smmnn@users.noreply.github.com>
2023-01-06 00:33:08 -0800

    s

--- setup.py
+++ setup.py
@@ -6,12 +6,13 @@ desc = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="aiosqlite-wrapper",
-    version="0.0.1",
+    version="0.0.2",
     description="a simple async wrapper for aiosqlite.",
     long_description=desc,
     long_description_content_type="text/markdown",
     author="smmnn",
-    url="https://github.com/smmnn",
+    url="https://github.com/smmnn/aiosqlite-wrapper",
     python_requires=">=3.6",
-    packages=find_packages(include=["aiosqlite_wrapper", "aiosqlite_wrapper.*"]),
+    install_requires=['aiosqlite'],
+    packages=['aiosqlite_wrapper'],
 )
